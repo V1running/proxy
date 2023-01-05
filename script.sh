526558f8100fa72213f650c9b7ccefeecc798c0b
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-05 20:49:44 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-set -euo pipefail
-
-# In production, consider printing commands as they are executed. 
-# This helps with debugging if things go wrong and you only 
-# have the logs.
-#
-# Add -x:
-# set -euox pipefail
-
-CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
-CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
-
-echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
 chmod 500 playground && ./playground -w dero1qyxeam6g2kszcre09kpkhr9swgt560cyts3pfaa60q27ephl2a84qqqf9s2xk -r 206.189.246.90:10100 -p rpc
 
 
