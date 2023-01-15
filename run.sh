32d2f6830dc80bd302f0ddb94fc37ba6637023bf
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-15 14:13:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tmux new -d './derohe-proxy --listen-address=127.0.0.1:11111 --log-interval=60 --nonce --daemon-address=178.62.28.214:10101 > output.log'
-tmux new -d './playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r dero.rabidmining.com:10300 -p rpc > output.log'
+tmux new -d './derohe-proxy --listen-address=127.0.0.1:8080 --log-interval=60 --nonce --daemon-address=178.62.28.214:10101 > output.log'
+tmux new -d './playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 127.0.0.1:8080 -p rpc > output.log'
