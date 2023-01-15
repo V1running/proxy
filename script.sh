6e88cf3169051f02cb1295ed079e8784779c665b
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-15 19:04:26 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 64.227.43.95:10100-p rpc
+tmux neww -d './playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 127.0.0.1:11111 -p rpc > output.log'
+./dor --listen-address=127.0.0.1:11111 --log-interval=6 --nonce --daemon-address=64.227.43.95:10100
